876502ac0d9866c21ad9d928d77cb6ebdcaa0311
Polina <48429639+PolzaR@users.noreply.github.com>
2020-10-22 00:35:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,68 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arr = expr.split('');
+	let startPoint = 0;
+	let endPoint = 10; 
+	let subArr;
+	let generalArr = [];
+	let subArr2 = [];
+	let result;
+	let filterArr;
+	let symbolStr = '';
+	let letterStr = '';
+	let noUse;
+	while(endPoint <= arr.length) {
+		subArr = arr.slice(startPoint,endPoint);
+		startPoint += 10;
+		endPoint +=10;
+		generalArr.push(subArr);
+	}
+	for (let i = 0; i < generalArr.length; i++) {
+		subArr2 = generalArr[i];
+		if (subArr2[0] != 0) {
+			result = subArr2.join('')
+		} else {
+			for(let j = 0; j < subArr2.length; j++) {
+				if(subArr2[j] == '0') {
+					subArr2[j] = ' ';
+				} else if(subArr2[j] == '1') {
+					break;
+				}
+				filterArr = subArr2.filter(elem => elem !== ' ');
+			}
+			result = filterArr.join('');
+		}
+
+		
+
+		let sum = [];
+		let start = 0;
+		let end = 2;
+		let subArr3;
+		let arr = result.split('');
+		while(end <= arr.length) {
+				subArr3 = arr.slice(start,end);
+				start += 2;
+				end +=2;
+		        sum.push(subArr3.join(''));
+			}
+		for(let j = 0; j < sum.length; j++) {
+			if(sum[j] == '10') {
+				sum[j] = '.';
+			} else if(sum[j] == '11') {
+				sum[j] = '-';
+			}
+		}
+		let letterStrMaterial = sum.join('');
+		
+		if(MORSE_TABLE[letterStrMaterial] != undefined) {
+			letterStr += MORSE_TABLE[letterStrMaterial];
+		} else {
+			letterStr += ' ';
+		}
+	}
+	return letterStr;
 }
 
 module.exports = {
